741bcd318ea5d7c91df3a7740b086526c1207c97
alisonramer <alisonramer@gmail.com>
2017-01-16 04:58:43 -0800

    updates to several files, organizing the DOM and HTML page.

---
+++ README.md
@@ -0,0 +1,5 @@
+# portfolio
+
+Inspired by Brian Nations (https://codepen.io/briannations/pen/bperqP)
+
+Inspired by Yana Redenska https://www.github.com/radenska
---
+++ public/scripts/articleView.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const articleView = {};
+
+articleView.populateFilters = function() {
+  $('article').each(function() {
+    if (!$(this).hasClass('template')) {
+      let val = $(this).find('address a').text();
+      let optionTag = `<option value="${val}">${val}</option>`;
+
+      if ($(`#author-filter option[value="${val}"]`).length === 0) {
+        $('#author-filter').append(optionTag);
+      }
+
+      val = $(this).attr('data-category');
+      optionTag = `<option value="${val}">${val}</option>`;
+      if ($(`#category-filter option[value="${val}"]`).length === 0) {
+        $('#category-filter').append(optionTag);
+      }
+    }
+  });
+};
+
+articleView.handleCategoryFilter = function() {
+  $('#category-filter').on('change', function() {
+    if ($(this).val()) {
+      $('article').hide();
+      $(`article[data-category="${$(this).val()}"]`).fadeIn();
+    } else {
+      $('article').fadeIn();
+      $('article.template').hide();
+    }
+    $('#author-filter').val('');
+  });
+};
+
+articleView.setTeasers = function() {
+  $('.article-body *:nth-of-type(n+2)').hide();
+
+  $('#articles').on('click', 'a.read-on', function(e) {
+    e.preventDefault();
+    $(this).parent().find('*').fadeIn();
+    $(this).hide();
+  });
+};
+
+
+articleView.initIndexPage = function() {
+  Article.all.forEach(function(a) {
+    $('#articles').append(a.toHtml())
+  });
+
+  articleView.populateFilters();
+  articleView.handleCategoryFilter();
+  articleView.handleMainNav();
+  articleView.setTeasers();
+};
