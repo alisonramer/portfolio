071e6515465f57fe202727a3e1b61dc5d1af76f6
alisonramer <alisonramer@gmail.com>
2017-02-01 02:21:14 -0800

    handlebars

--- public/scripts/articleView.js
+++ public/scripts/articleView.js
@@ -1,27 +1,13 @@
 'use strict';
 
-const articleView = {};
-
-articleView.handleMainNav = function () {
+function articleView = () {
   $('.main-nav').on('click', '.tab', function() {
     $('.tab-content').hide();//this is going to hide the tab content because the user clicked on a tab.
     var thingClicked = $(this).attr('data-content');
     $('#' + thingClicked).fadeIn();
   });
-  $('.main-nav .tab:first').click();//this is what makes it so that when you go to the page, it shows the articles -- that's the first tab 
+  $('.main-nav .tab:first').click();//this is what makes it so that when you go to the page, it shows the articles -- that's the first tab
   //
 };
 
-articleView.initIndexPage = function() {
-
-  Article.all.forEach(function(article) {
-    console.log('log article', article)
-    $('#articles').append(article.toHtml())
-  });
-
-  articleView.handleMainNav();
-  console.log('getting to handleMainNav')
-  // articleView.populateFilters();
-  // articleView.handleCategoryFilter();
-  // articleView.setTeasers();
-};
+articleView();
