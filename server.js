b44f6a48eefb88c535fd822fd59c9e6bd99e434f
alisonramer <alisonramer@gmail.com>
2017-02-11 18:24:14 -0800

    fixing node server issue

--- server.js
+++ server.js
@@ -3,9 +3,9 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
-app.use(express.static('/public'));
+app.use(express.static('./public'));
 app.get('*', function(request, response) {
-  response.sendFile('/index.html', {root: '.'})
+  response.sendFile('./public', {root: '.'})
 });
 app.listen(PORT, function(){
   console.log('Server is up and running on port 4000 and can be accessed at local host 4000 in your browser');
